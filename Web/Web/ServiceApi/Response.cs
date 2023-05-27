0dd012cc3e9b6494109c4da660a65327471c420c
lucasmaritanwhitaker <lucasmaritanlab@gmail.com>
2023-05-27 12:53:24 -0300

    Organizando o ambiente

---
+++ Web/Web/ServiceApi/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.ServiceApi
+{
+    public static class Response
+    {
+        //public static T GetModel<T>(this HttpResponseMessage request) => JsonContent.DeserializeObject<T>(request.Content.ReadAsStringAsync().Result);
+        public static string GetMessage(this HttpResponseMessage request) => request.Content.ReadAsStringAsync().Result;
+    }
+}
